683ab583c309a699b2b205b21b1ee1ae4031b254
Fakhri <fakhrialuwe@rocketmail.com>
2020-12-09 16:01:32 +0800

    Fixes #3: Created all DB migrations

---
+++ migrations/m201207_165516_create_table_user.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201207_165516_create_table_user
+ */
+class m201207_165516_create_table_user extends Migration
+{
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function safeUp()
+    // {
+
+    // }
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function safeDown()
+    // {
+    //     echo "m201207_165516_create_table_user cannot be reverted.\n";
+
+    //     return false;
+    // }
+
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey()->unsigned(),
+            'uid' => $this->string(60)->unique()->notNull(),
+            'username' => $this->string(45)->notNull(),
+            'email' => $this->string(255)->unique()->notNull(),
+            'password' => $this->string(60)->notNull(),
+            'status' => $this->integer(4)->notNull()->defaultValue(0),
+            'contact_email' => $this->boolean()->notNull()->defaultValue(false),
+            'contact_phone' => $this->boolean()->notNull()->defaultValue(false),
+            'created' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP')
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+    
+}
